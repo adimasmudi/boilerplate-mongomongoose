24db10fc615394ad3c498b1f4c645ea5331655c5
adimasmudi <adimasmudi@gmail.com>
2022-07-02 17:31:16 +0700

    copas boss

--- myApp.js
+++ myApp.js
@@ -63,17 +63,21 @@ const findPersonById = (personId, done) => {
 };
 
 const findEditThenSave = (personId, done) => {
-  const foodToAdd = "hamburger";
-  Person.findById({_id:personId}, function(err, person){
-    if(err) return console.log(err)
-    person.favoriteFoods.push(foodToAdd)
-    person.save(function(err, updatedPerson){
-      if(err) return console.log(err)
-      done(updatedPerson)
+  const foodToAdd = 'hamburger';
+
+  // .findById() method to find a person by _id with the parameter personId as search key. 
+  Person.findById(personId, (err, person) => {
+    if(err) return console.log(err); 
+  
+    // Array.push() method to add "hamburger" to the list of the person's favoriteFoods
+    person.favoriteFoods.push(foodToAdd);
+
+    // and inside the find callback - save() the updated Person.
+    person.save((err, updatedPerson) => {
+      if(err) return console.log(err);
+      done(null, updatedPerson)
     })
-    
-    
-})
+  })
 
   
 };
