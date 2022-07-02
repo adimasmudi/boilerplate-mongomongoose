313a407a1b639dfd16289948691380a2754033d1
adimasmudi <adimasmudi@gmail.com>
2022-07-02 17:56:29 +0700

    finish

--- myApp.js
+++ myApp.js
@@ -117,8 +117,16 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
+  Person
+    .find({favoriteFoods:foodToSearch})
+    .sort('name')
+    .limit(2)
+    .select({age:0})
+    .exec((err, data)=>{
+      if(err) return console.log(err)
+      done(null, data)
+  })
 
-  done(null /*, data*/);
 };
 
 /** **Well Done !!**
